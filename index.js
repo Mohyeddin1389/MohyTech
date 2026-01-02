77f32d1b2d4fefde463df79c4af60e70504d6a27
Mohyeddin1389 <testlolantmanxx@gmail.com>
2026-01-02 22:31:50 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+// // let number = prompt("یک عدد وارد کنید:");
+
+// // number = Number(number);
+
+// // if (number % 2 === 0) {
+// //   alert("عدد زوج است");
+// // } else {
+// //   alert("عدد فرد است");
+// // }
+
+// // let a = Number(prompt("ضلع اول را وارد کنید:"));
+// // let b = Number(prompt("ضلع دوم را وارد کنید:"));
+// // let c = Number(prompt("ضلع سوم را وارد کنید:"));
+
+// // if (a + b > c && a + c > b && b + c > a) {
+// //   alert("می‌توان مثلث ساخت");
+// // } else {
+// //   alert("نمی‌توان مثلث ساخت");
+// // }
+
+// let day = Number(prompt("عدد 1 تا 7 وارد کنید:"));
+
+// switch (day) {
+//   case 1:
+//     alert("شنبه");
+//     break;
+//   case 2:
+//     alert("یکشنبه");
+//     break;
+//   case 3:
+//     alert("دوشنبه");
+//     break;
+//   case 4:
+//     alert("سه‌شنبه");
+//     break;
+//   case 5:
+//     alert("چهارشنبه");
+//     break;
+//   case 6:
+//     alert("پنجشنبه");
+//     break;
+//   case 7:
+//     alert("جمعه");
+//     break;
+//   default:
+//     alert("عدد نامعتبر است");
+// }
